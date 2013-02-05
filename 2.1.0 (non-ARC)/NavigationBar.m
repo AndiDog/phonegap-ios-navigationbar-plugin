fb7c9fbadd734b14c3ccc47f65cae859e0e6c031
Till Zimmermann <tillz@tillz.net>
2013-02-05 01:30:28 +0100

    removed unused code

--- 2.1.0 (non-ARC)/NavigationBar.m
+++ 2.1.0 (non-ARC)/NavigationBar.m
@@ -21,8 +21,6 @@
 #endif
 @synthesize navBarController;
 
-#define UIColorFromRGB(rgbValue) [UIColor colorWithRed:((float)((rgbValue & 0xFF0000) >> 16))/255.0 green:((float)((rgbValue & 0xFF00) >> 8))/255.0 blue:((float)(rgbValue & 0xFF))/255.0 alpha:1.0]
-
 -(CDVPlugin*) initWithWebView:(UIWebView*)theWebView
 {
     self = (NavigationBar*)[super initWithWebView:theWebView];
